7f211373550837e795fa488eba216421b0dc41eb
PANCHAL AKSHAY <panchalakshay6277@gmail.com>
2024-05-14 11:27:27 +0530

    React Functional Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import hamburger from './hamburger.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <nav>
+          <div className="logo">
+            <h1>Website</h1>
+          </div>
+          <ul id="menu">
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+            <li>Faqs</li>
+          </ul>
+          <img className="hamburger" id="hamburger" src={hamburger} alt="" />
+        </nav>
+      </div>
     </div>
   );
 }
