12daf9f0482b58d3a28393d321fe593e1b5dcceb
PANCHAL AKSHAY <panchalakshay6277@gmail.com>
2024-05-14 12:05:50 +0530

    class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import './App.css';
-import hamburger from './hamburger.svg';
+import User from './user';
 
 function App() {
+
+  function common() {
+    return <p>Common component</p>
+  }
+
   return (
-    <div className="App">
-      <div className="container">
-        <nav>
-          <div className="logo">
-            <h1>Website</h1>
-          </div>
-          <ul id="menu">
-            <li>Home</li>
-            <li>About</li>
-            <li>Services</li>
-            <li>Contact</li>
-            <li>Faqs</li>
-          </ul>
-          <img className="hamburger" id="hamburger" src={hamburger} alt="" />
-        </nav>
-      </div>
-    </div>
+    <>
+      <div className="App">
+        <h1>Hello World!</h1>
+      </div >
+      <User />
+      {common()}
+    </>
   );
 }
 
