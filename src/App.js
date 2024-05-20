e153e0eb57bb892eb232fad22e31f2690ee89d68
PANCHAL AKSHAY <panchalakshay6277@gmail.com>
2024-05-20 15:52:35 +0530

    Refactor and remove unused components

--- src/App.js
+++ src/App.js
@@ -1,39 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import './App.css';
-import UserTotal from './Components/UserTotal';
-function App(props) {
-
-
-  const [count, setCount] = useState(0);
-
-  const [total, setTotal] = useState(100);
-
-
-  useEffect(() => {
-    // console.log("After render Count !!");
-
-  }, [count])
-
-  useEffect(() => {
-    // console.log("After render Total !!");
-
-  }, [total])
-
-
+function App() {
   return (
     <>
       <div className="App">
-        <h1>useEffect Condition Hook</h1>
-
-        <UserTotal count={count} total={total} />
-
-        {/* <h2>Count: {count}</h2> */}
-
-        <button onClick={() => setCount(count + 1)} >Update Count</button>
-
-        {/* <h2>Total: {total}</h2> */}
-
-        <button onClick={() => setTotal(total + 1)} >Update Total</button>
+        <h1>Styles Type in React.</h1>
       </div>
     </>
   );
