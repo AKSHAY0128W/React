36ee7cec04e11a3c58eddff80be30948c254c6c9
PANCHAL AKSHAY <panchalakshay6277@gmail.com>
2024-05-20 16:12:54 +0530

    learn styling in React

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import React from 'react';
 import './App.css';
+
+// Example 2
+import './styles.css'
+
+// Example 4
+import Style from './style.module.css'
+
+// Example 5
+
+import "./style.scss"
+
 function App() {
+
+
+  // Example 3
+
+  const headline = {
+    backgroundColor: "blue",
+    color: "$333",
+    padding: "10px"
+  }
+
   return (
     <>
       <div className="App">
         <h1>Styles Type in React.</h1>
+
+        {/* Example 1 Inline Css */}
+
+        <h1 style={{ color: "red", backgroundColor: "green", border: "20px solid black" }}>Inline Method</h1>
+
+        {/* Example 2 Normal Css */}
+        <h1 className='primary'>Normal Method</h1>
+
+        {/* Example 3 CSS in JS */}
+        <h1 style={headline}>CSS in JS</h1>
+
+        {/* Example 4 CSS in Module */}
+        <h1 className={Style.title}>Css In Module</h1>
+
+        {/* Example 5 CSS in Sass */}
+        <h1 className="primary-sass">sass & <span>scss</span> In Module</h1>
+
+
       </div>
     </>
   );
