789fb1d6f60a6b4bdb4cef4050a9bd0a8a17706a
PANCHAL AKSHAY <panchalakshay6277@gmail.com>
2024-05-21 13:33:04 +0530

    Learn Array Map Function

--- src/App.js
+++ src/App.js
@@ -1,22 +1,72 @@
 import React from 'react';
 import './App.css';
-// boostrap
-import Button from 'react-bootstrap/Button';
 
 function App() {
+
+  // Example 1
+  const names = ["Alice", "bob", "charlie", "bhaskar"]
+
+
+  // Example 2
+
+  const products = [
+    {
+      id: 1,
+      name: "Product 1",
+      price: 100
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      rice: 200
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      price: 300
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      price: 400
+    }
+  ]
+
+  // Example 3 return map function
+
+  const fruits = ["Apple", "Banana", "Orange", "Mango"]
+
+  const fruitsItems = fruits.map((fruit, index) => <li key={index}>{fruit}</li>)
+
   return (
     <>
       <div className="App">
-        <h1>Install Bootstrap In React.</h1>
-        <Button variant="primary">Primary</Button>{' '}
-        <Button variant="secondary">Secondary</Button>{' '}
-        <Button variant="success">Success</Button>{' '}
-        <Button variant="warning">Warning</Button>{' '}
-        <Button variant="danger">Danger</Button>{' '}
-        <Button variant="info">Info</Button>{' '}
-        <Button variant="light">Light</Button>{' '}
-        <Button variant="dark">Dark</Button>
-        <Button variant="link">Link</Button>
+        <h1>Array With Map Function.</h1>
+
+        {/* Example 1 Array */}
+
+        <ul>
+          {
+            names.map((name, index) => (
+              <li key={index}>{name}</li>
+            ))
+          }
+        </ul>
+
+
+        {/* Example 2 Array Of Objects */}
+
+        <ul>
+          {
+            products.map((product) => (
+              <li key={product.id}>{product.name} - ${product.price}</li>
+            ))
+          }
+        </ul>
+
+
+        {/* Example 3 return map function */}
+        <ul>{fruitsItems}</ul>
       </div>
     </>
   );
