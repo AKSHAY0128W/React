d60f2bf94302c123a4f0a80c012d25611462ed1d
PANCHAL AKSHAY <panchalakshay6277@gmail.com>
2024-05-21 14:36:46 +0530

    Learn Bootstrap Table

--- src/App.js
+++ src/App.js
@@ -1,22 +1,72 @@
 import React from 'react';
 import './App.css';
-// boostrap
-import Button from 'react-bootstrap/Button';
-
+import Table from 'react-bootstrap/Table';
 function App() {
+
+  const tableData = [
+    {
+      id: 1,
+      name: 'John Doe',
+      age: 25,
+      location: 'USA'
+    },
+    {
+      id: 2,
+      name: 'Jane Doe',
+      age: 25,
+      location: 'UK'
+    },
+    {
+      id: 3,
+      name: 'Tom Cruise',
+      age: 45,
+      location: 'JAPAN'
+    },
+    {
+      id: 4,
+      name: 'Brad Pitt',
+      age: 35,
+      location: 'INDIA'
+    },
+    {
+      id: 5,
+      name: 'Angelina Jolie',
+      age: 30,
+      location: 'FRANCE'
+    }
+  ]
+
   return (
     <>
       <div className="App">
-        <h1>Install Bootstrap In React.</h1>
-        <Button variant="primary">Primary</Button>{' '}
-        <Button variant="secondary">Secondary</Button>{' '}
-        <Button variant="success">Success</Button>{' '}
-        <Button variant="warning">Warning</Button>{' '}
-        <Button variant="danger">Danger</Button>{' '}
-        <Button variant="info">Info</Button>{' '}
-        <Button variant="light">Light</Button>{' '}
-        <Button variant="dark">Dark</Button>
-        <Button variant="link">Link</Button>
+        <h1>Bootstarp Table in React.</h1>
+        <div>
+          <Table striped="columns" responsive>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Age</th>
+                <th>Location</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                tableData.map((data) => (
+                  data.age > 30 ?
+
+                    <tr key={data.id}>
+                      <td>{data.id}</td>
+                      <td>{data.name}</td>
+                      <td>{data.age}</td>
+                      <td>{data.location}</td>
+                    </tr>
+                    : null
+                ))
+              }
+            </tbody>
+          </Table>
+        </div>
       </div>
     </>
   );
