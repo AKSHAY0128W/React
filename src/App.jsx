be79900eba51fca10b8a0a759fb14be542d51831
PANCHAL AKSHAY <panchalakshay6277@gmail.com>
2024-05-24 16:08:34 +0530

    Learn HOC in React.

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,60 @@
-import React, { useRef } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 function App() {
-
-
   // Example 1
 
-  const inputRef = useRef(null)
-  const inputRef2 = useRef(null)
 
-  const submitForm = (e) => {
-    e.preventDefault();
-    console.log("First Input Value:", inputRef.current.value);
-    console.log("Second Input Value:", inputRef2.current.value);
+  // HOC
 
-    let input3 = document.getElementById("input").value;
-    console.log("Third Input Value:", input3);
 
+  const withCounter = (WrappedComponent) => {
+    return function WithCounter(props) {
+      const [count, setCount] = useState(0)
+      const increment = () => {
+        setCount(count + 1)
+      }
+      return (
+        <WrappedComponent {...props} count={count} increment={increment} />
+      )
+    }
   }
 
 
 
+  // Functional
+
+  const Counter = ({ count, increment }) => {
+
+    return (
+      <div>
+        <p>Count:{count}</p>
+        <button onClick={increment}>Increment</button>
+      </div>
+    )
+  }
+
+
+
+  // Wrap Counter Componetn with withCounter HOC
+
+  const CounterWithEnhanceMent = withCounter(Counter);
+
   return (
     <div className='App'>
 
-      <h1>UnControlled Component in React.</h1>
-      <form action="" onSubmit={submitForm}>
-        <input type="text" ref={inputRef} />
-        <input type="text" ref={inputRef2} />
-        <input type="text" id='input' />
-        <button>Submit</button>
-      </form>
+      <h1>Higher Order Component in React.</h1>
 
+      {/* Example 1 */}
+
+      {/* <h1>Count:{count}</h1>
+
+      <button onClick={() => setCount(count + 14)}>Count ++</button> */}
+
+
+
+      {/* Example 2 */}
+
+      <CounterWithEnhanceMent />
     </div>
   )
 }
