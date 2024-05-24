ffdd30352ee0af8d8dd1b273f6a00e16cea97598
PANCHAL AKSHAY <panchalakshay6277@gmail.com>
2024-05-24 12:29:32 +0530

    Learn Uncontrolled Component in React with useRef hook and JS DOM method by id

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
-import React, { useState } from 'react'
+import React, { useRef } from 'react'
 import './App.css'
 function App() {
 
+
   // Example 1
 
-  const [inputValue, setInputValue] = useState("")
+  const inputRef = useRef(null)
+  const inputRef2 = useRef(null)
 
-  const handleChange = (e) => {
-    setInputValue(e.target.value)
-    console.log(inputValue);
+  const submitForm = (e) => {
+    e.preventDefault();
+    console.log("First Input Value:", inputRef.current.value);
+    console.log("Second Input Value:", inputRef2.current.value);
+
+    let input3 = document.getElementById("input").value;
+    console.log("Third Input Value:", input3);
+
   }
+
+
 
   return (
     <div className='App'>
 
-      <h1>Controlled Component in React.</h1>
+      <h1>UnControlled Component in React.</h1>
+      <form action="" onSubmit={submitForm}>
+        <input type="text" ref={inputRef} />
+        <input type="text" ref={inputRef2} />
+        <input type="text" id='input' />
+        <button>Submit</button>
+      </form>
 
-      <input type="text" value={inputValue} onChange={handleChange} />
-
-      <p>Value:{inputValue}</p>
     </div>
   )
 }
