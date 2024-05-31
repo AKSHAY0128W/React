c4e765a0511cc0e4ad80f067c9a8afc537ff6ec6
PANCHAL AKSHAY <panchalakshay6277@gmail.com>
2024-05-31 12:43:21 +0530

    Learn useTransition hook in React.

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,57 @@
-import React, { useState, useSyncExternalStore } from 'react'
+import React, { useEffect, useState, useTransition } from 'react'
 import './App.css'
 
 
 function App() {
 
   // Example 1
+  const [isPending, startTransition] = useTransition()
+  const [count, setCount] = useState(0) // Use useState here, not useEffect
 
-  const width = useSyncExternalStore((listener) => {
-    window.addEventListener("resize", listener);
-    return () => {
-      window.removeEventListener("resize", listener)
-    }
-  }, () => window.innerWidth)
+
+  // Example 2
+
+  const [input, setInput] = useState()
+  const [dataList, setDataList] = useState([])
+
+
+  const dataSize = 10000;
+
+  // Function 
+  const handleChange = (e) => {
+
+    setInput(e.target.value);
+    startTransition(() => {
+      const a = [];
+      for (let i = 0; i < dataSize; i++) {
+        a.push(e.target.value)
+      }
+      setDataList(a);
+    })
+  };
+
 
 
   return (
     <div className='App'>
+      <h1>useTransition hook in React.</h1>
 
-      <h1>useSyncExternalStore hook in React.</h1>
-      {/* Example 1 */}
-      <p>Window Size : {width}</p>
+      {/* Example 2 */}
+
+      <input type="text" value={input} onChange={handleChange} />
+      {
+        isPending ?
+          <div className="spinner-border mt-5 text-primary" role="status">
+            <span className="sr-only"></span>
+          </div> : // Display a spinner when isPending is true
+          dataList.map((item, index) => {
+            return <div key={index}>{item}</div>
+          })
+      }
+
+
     </div >
   )
 }
 
-export default App
+export default App  
