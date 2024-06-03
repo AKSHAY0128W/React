c668134f3244287ecb5b3ac71bc3d8d15ef4fa32
PANCHAL AKSHAY <panchalakshay6277@gmail.com>
2024-06-03 17:39:35 +0530

    Learn Fetch Api POST Method in React.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState, useDeferredValue } from 'react'
 import './App.css'
-import CounterComp from './CounterComp'
-import GetMethod from './GetMethod'
+import PostMethod from './PostMethod'
 function App() {
   return (
     <div className='App'>
-      <h1>Fetch Api GET Method in React.</h1>
+      <h1>Fetch Api Post Method in React.</h1>
 
-      <GetMethod />
+      {/* Example 1 */}
+      <PostMethod />
     </div >
   )
 }
