ee6d252562853246344dbb233bc9482724163844
PANCHAL AKSHAY <panchalakshay6277@gmail.com>
2024-06-03 17:11:25 +0530

    Learn Fetch Api Get Method in React.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState, useDeferredValue } from 'react'
 import './App.css'
 import CounterComp from './CounterComp'
+import GetMethod from './GetMethod'
 function App() {
   return (
     <div className='App'>
-      <h1>custom hook in React.</h1>
+      <h1>Fetch Api GET Method in React.</h1>
 
-      <CounterComp />
+      <GetMethod />
     </div >
   )
 }
