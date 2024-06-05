c0435a6c2ef184670fee42d5c20edc03a84d971e
PANCHAL AKSHAY <panchalakshay6277@gmail.com>
2024-06-05 16:54:15 +0530

    Learn fetch API DELETE in React.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState, useDeferredValue } from 'react'
 import './App.css'
-import UpdateMethod from './UpdateMethod'
+import DeleteMethod from './DeleteMethod'
 function App() {
   return (
     <div className='App'>
-      <h1>Fetch Api Put Method in React.</h1>
+      <h1>Fetch Api Delete Method in React.</h1>
 
       {/* Example 1 */}
-      <UpdateMethod />
+      <DeleteMethod />
     </div >
   )
 }
